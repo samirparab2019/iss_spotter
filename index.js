f58aaa9fbf7157cb1b902ea7f9e4a7315d2ba95d
Samir Parab <samirparab@yahoo.com>
2019-10-25 02:39:41 +0000

    files added..

---
+++ index.js
@@ -0,0 +1,50 @@
+// index.js
+
+// The code below is temporary and can be commented out.
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
+
+
+fetchCoordsByIP('162.245.144.188', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned Coords:' , coords);
+});
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
+
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(passTimes);
+});
+
+
