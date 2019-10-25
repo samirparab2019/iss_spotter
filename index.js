6b77d8015c59036130e51290cfb9a98d7bb179cf
Samir Parab <samirparab@yahoo.com>
2019-10-25 12:08:37 +0000

    index2.js and iss_promised.js addedd.

--- index.js
+++ index.js
@@ -37,7 +37,27 @@ fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   console.log('It worked! Returned flyover times:' , passTimes);
 });
 
+//const { nextISSTimesForMyLocation } = require('./iss');
 
+/** 
+ * Input: 
+ *   Array of data objects defining the next fly-overs of the ISS.
+ *   [ { risetime: <number>, duration: <number> }, ... ]
+ * Returns: 
+ *   undefined
+ * Sideffect: 
+ *   Console log messages to make that data more human readable.
+ *   Example output:
+ *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+ */
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
@@ -46,5 +66,6 @@ nextISSTimesForMyLocation((error, passTimes) => {
   // success, print out the deets!
   console.log(passTimes);
 });
+
 
 
